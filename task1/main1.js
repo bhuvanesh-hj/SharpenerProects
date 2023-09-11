836a4d1771fad579185d7d9c7bdc61ba66a7d82b
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-09-12 00:10:42 +0530

    task 3

--- task1/main1.js
+++ task1/main1.js
@@ -7,8 +7,25 @@
 // add[0].textContent='ADD ITEMS';
 // add[0].style.color ='green';
 
-var list3 = document.getElementsByClassName('list-group-item')
-list3[2].style.backgroundColor='green'
-for(var i=0;i<list3.length;i++){
-    list3[i].style.fontWeight='Bold'
-}
+// var list3 = document.getElementsByClassName('list-group-item')
+// list3[2].style.backgroundColor='green'
+// for(var i=0;i<list3.length;i++){
+//     list3[i].style.fontWeight='Bold'
+// }
+
+
+// var list = document.getElementsByClassName('ist-group-item')
+
+
+var list1 = document.getElementsByTagName('li')
+
+list1[0].textContent='Hello 1'
+list1[0].style.backgroundColor='red'
+list1[0].style.color='white'
+list1[2].style.backgroundColor='red'
+list1[1].style.fontFamily='poppings'
+list1[1].style.color='blue'
+for(var i=0;i<list1.length;i++){
+        list1[i].style.fontWeight='Bold'
+    }
+list1[4].style.backgroundColor='yellow'
