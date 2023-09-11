0a6bd88a0f3fac4bc7f91369c6bcb7ee31a6089f
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-09-11 17:42:02 +0530

    task 1

---
+++ task1/main1.js
@@ -0,0 +1,8 @@
+const headElement = document.getElementById('header-title')
+
+headElement.style.borderBottom ='solid 2px #000';
+
+var add = document.getElementsByClassName('title');
+
+add[0].textContent='ADD ITEMS';
+add[0].style.color ='green';
