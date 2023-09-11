517c046a2a2028f45eb47b8759c6652450e11b8b
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-09-11 23:24:09 +0530

    task 2 added

--- task1/main1.js
+++ task1/main1.js
@@ -1,8 +1,14 @@
-const headElement = document.getElementById('header-title')
+// const headElement = document.getElementById('header-title')
 
-headElement.style.borderBottom ='solid 2px #000';
+// headElement.style.borderBottom ='solid 2px #000';
 
-var add = document.getElementsByClassName('title');
+// var add = document.getElementsByClassName('title');
 
-add[0].textContent='ADD ITEMS';
-add[0].style.color ='green';
+// add[0].textContent='ADD ITEMS';
+// add[0].style.color ='green';
+
+var list3 = document.getElementsByClassName('list-group-item')
+list3[2].style.backgroundColor='green'
+for(var i=0;i<list3.length;i++){
+    list3[i].style.fontWeight='Bold'
+}
